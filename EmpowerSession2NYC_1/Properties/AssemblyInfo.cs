c32f84039542b0fe1678486cdc533fbf1650074a
cfclasvegas <cfclasvegas@gmail.com>
2023-05-16 11:31:42 -0700

    Add project files.

---
+++ EmpowerSession2NYC_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerSession2NYC_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerSession2NYC_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("59091DCF-610E-4CD9-9DA7-C9B9684B3658")]
+[assembly: AssemblyVersion("1.0.0.0")]
